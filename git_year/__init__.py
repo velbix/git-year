ad016cc8aa05cb7bafcc9ccbf017796d33c0247f
Serge <serge@velbix.com>
2025-11-20 21:13:25 -0500

    fix version error message in __init__.py

--- git_year/__init__.py
+++ git_year/__init__.py
@@ -3,4 +3,4 @@ from importlib import metadata
 try:
     __version__ = metadata.version("git-year")
 except metadata.PackageNotFoundError:
-    __version__ = "unknown (metadata unavailable)"
+    __version__ = "- version unknown (metadata unavailable)"
